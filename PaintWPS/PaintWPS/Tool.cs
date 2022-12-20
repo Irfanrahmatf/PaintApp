b3fecc7540e30791d44b2f93c1a5d405ace4f4e4
Irfan Rahmat Firmansyah <90383815+Irfanrahmatf@users.noreply.github.com>
2022-12-20 15:54:53 +0700

    Add files via upload

---
+++ PaintWPS/PaintWPS/Tool.cs
@@ -0,0 +1,10 @@
+﻿namespace PaintWPS
+{
+    internal class Tool
+    {
+        public static object Fill { get; internal set; }
+        public static object Pencil { get; internal set; }
+        public static object Eraser { get; internal set; }
+        public static object Text { get; internal set; }
+    }
+}
---
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Tool tool { get; set; }
+        public Point startpoint { get; private set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            tool = Tool.Pencil;
+        }
+        enum Tool
+        {
+            Pencil,
+            Eraser,
+            Fill,
+            Text
+        }
+
+        private void btnPencil_Click(object sender, RoutedEventArgs e)
+        {
+            // Set the tool to pencil
+            tool = Tool.Pencil;
+        }
+
+        private void btnEraser_Click(object sender, RoutedEventArgs e)
+        {
+            // Set the tool to eraser
+            tool = Tool.Eraser;
+        }
+
+        private void btnFill_Click(object sender, RoutedEventArgs e)
+        {
+            // Set the tool to fill
+            tool = Tool.Fill;
+        }
+
+        private void btnText_Click(object sender, RoutedEventArgs e)
+        {
+            // Set the tool to text
+            tool = Tool.Text;
+        }
+
+        private void canvas_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            // Store the current mouse position
+            startpoint = e.GetPosition(canvas);
+        }
+
+        private void canvas_MouseMove(object sender, MouseEventArgs e)
+        {
+        }
+}
