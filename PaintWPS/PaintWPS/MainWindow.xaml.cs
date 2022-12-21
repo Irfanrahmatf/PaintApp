d8fda67b4c7589f80839c3595013a8333c8ceeae
Irfan Rahmat Firmansyah <90383815+Irfanrahmatf@users.noreply.github.com>
2022-12-21 21:35:45 +0700

    Add files via upload

--- PaintWPS/PaintWPS/MainWindow.xaml.cs
+++ PaintWPS/PaintWPS/MainWindow.xaml.cs
@@ -1,23 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace PaintWPS
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    /// 
+
     public partial class MainWindow : Window
     {
         public object tool { get; private set; }
@@ -28,7 +17,7 @@ namespace PaintWPS
             InitializeComponent();
             tool = Tool.Pencil;
         }
-            enum Tool
+        enum Tool
         {
             Pencil,
             Eraser,
@@ -59,19 +48,5 @@ namespace PaintWPS
             // Set the tool to text
             tool = Tool.Text;
         }
-
-        private void canvas_MouseDown(object sender, MouseButtonEventArgs e)
-        {
-            // Store the current mouse position
-            startpoint = e.GetPosition(canvas);
-        }
-
-        private void canvas_MouseMove(object sender, MouseEventArgs e)
-        {
-
-
-
-
-        }
-
     }
+}
